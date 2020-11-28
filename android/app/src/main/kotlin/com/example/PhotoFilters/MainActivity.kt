2c66cf98f9a462b64f21637fb85b5e3417c5ca0e
Edenik <Edenik5@gmail.com>
2020-11-28 18:39:16 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/PhotoFilters/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.PhotoFilters
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter PhotoFilters',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter PhotoFilters'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Camera',
+        child: Icon(Icons.camera_alt),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
